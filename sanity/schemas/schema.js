4d1fdf66fa479f7a520bbb4df0035f807c68a1ca
bilginmrv <mrvblgn44@gmail.com>
2022-07-28 09:44:33 +0300

    Create pizza.js and schema.js files

--- sanity/schemas/schema.js
+++ sanity/schemas/schema.js
@@ -3,6 +3,7 @@ import createSchema from 'part:@sanity/base/schema-creator'
 
 // Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type'
+import Pizza from './Pizza'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -11,6 +12,7 @@ export default createSchema({
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
-    /* Your types here! */
+    Pizza
+   
   ]),
 })
