a45460403d6578aea541cfbc613496b2ffdf697b
bilginmrv <mrvblgn44@gmail.com>
2022-08-01 23:08:23 +0300

    Create order page

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,13 +1,18 @@
 import css from '../styles/Header.module.css'
 import Image from 'next/image'
 import Logo from '../assets/Logo.png'
-import {UilShoppingBag} from '@iconscout/react-unicons'
+import {UilShoppingBag, UilReceipt} from '@iconscout/react-unicons'
 import { useStore } from "../store/store"
 import Link from 'next/Link'
+import { useState } from 'react'
+import { useEffect } from 'react'
 
 export default function Header() {
 
-
+    const [Order, setOrder] = useState("")
+    useEffect(()=>{
+        setOrder(localStorage.getItem("order"))
+    }, [])
     const items = useStore((state)=>state.cart.pizzas.length)
     return(
         <div className={css.header}>
@@ -19,7 +24,10 @@ export default function Header() {
 
             {/* menu side */}
             <ul className={css.menu}>
-                <li>Home</li>
+                <li>
+                    <Link href='../'>Home</Link>
+                    
+                </li>
                 <li>Menu</li>
                 <li>Contact</li>
             </ul>
@@ -35,8 +43,19 @@ export default function Header() {
                     </div>
                 
                 </Link>
-                
+
+                {Order && (
+                    <Link href={`/order/${Order}`}>
+                        <div className={css.cart}>
+                            <UilReceipt size={35} color='#2E2E2E' />
+                            {Order != "" && <div className={css.badge}>1
+                        </div>}
+                        </div>
+                    
+                    </Link>
+                )}
             </div>
+
         </div>
     )
     
--- components/OrderModal.jsx
+++ components/OrderModal.jsx
@@ -4,9 +4,11 @@ import toast, {Toaster} from "react-hot-toast"
 import {useStore} from "../store/store";
 import { createOrder } from "../lib/orderHandler";
 import css from "../styles/OrderModal.module.css";
+import {useRouter} from 'next/router';
 
 export default function OrderModal({opened, setOpened, PaymentMethod}) {
     const theme = useMantineTheme();
+    const router = useRouter();
     const [FormData, setFormData] = useState({})
 
     const handleInput = (e)=> {
@@ -26,7 +28,9 @@ export default function OrderModal({opened, setOpened, PaymentMethod}) {
             typeof window !== 'undefined' && localStorage.setItem('order', id)
         }
 
-    }
+        router.push(`/order/${id}`)
+    };
+    
     return(
         <Modal
         overLayColor = {theme.colorScheme === 'dark' ? theme.colors.dark[9] : theme.colors.gray[2]}
